dae96f821db7696954fe0d69dc4a618cfe57f09e
fazilat990 <59200609+fazilat990@users.noreply.github.com>
2024-08-06 12:27:43 +0500

    commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -41,33 +41,30 @@ function Work() {
     </div>
     <div className="flex flex-wrap -mx-4">
       {filteredProjects.map(project => (
+        
         <div key={project.id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-8">
+          
           <div className="p-4 rounded relative bg-teal-400/10">
             <div className="text-white-600 absolute top-15 bg-[var(--bg)] p-2"> {project.category}</div>
             <div className="scrolling-img">
+            <a href={project.projectUrl} target="_blank" rel="noopener noreferrer" className="block">
               <img
                 src={require(`../assets/images/projects/${project.image}`)}
                 alt={project.title}
                 className="w-full h-auto mb-4 rounded max-h-72 object-cover object-top"
               />
+              </a>
             </div>
-            <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-            
-            <div className="tags">
+            <h3 className="text-xl font-semibold mb-2"><a href={project.projectUrl} target="_blank" rel="noopener noreferrer" className="block">{project.title}</a></h3>
+            <div className="tags flex items-center overflow-auto cursor-grabbing">
               {project.tags.map((tag, index) => (
-                    <span key={index} className='inline-flex items-center capitalize rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 mr-1 mb-1'>{tag}</span>
+                    <span key={index} className='inline-flex items-center capitalize rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 mr-1 mb-1 w-fit-content'>{tag}</span>
               ))}
             </div>
-            <a
-              href={project.projectUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white-500 hover:underline"
-            >
-              View Project
-            </a>
+            <a href={project.projectUrl} target="_blank" rel="noopener noreferrer" className="block">View Project</a>
           </div>
         </div>
+       
       ))}
     </div>
   </div> 
