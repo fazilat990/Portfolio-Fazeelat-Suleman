95e00d496fcacced8bdd5ede45f5d7799146516e
fazilat990 <59200609+fazilat990@users.noreply.github.com>
2025-02-01 17:29:55 +0500

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,7 +11,6 @@ import materialuiIcon from '../assets/images/materialui.svg';
 import wordpressIcon from '../assets/images/wordpress.svg';
 import shopifyIcon from '../assets/images/shopify.svg';
 import reactIcon from '../assets/images/react.svg';
-import ionicIcon from '../assets/images/ionic.svg'
 
 // DOC: https://www.npmjs.com/package/react-animated-progress-bar?activeTab=readme
 
@@ -66,11 +65,236 @@ function Skills() {
       skillname: 'React UI',
       skillPercentage: '50',
       skillIcon: reactIcon
+    }
+  ]
+  const additionalSkills = [
+    {
+      shortSkillName: 'Debugging'
     },
     {
-      skillname: 'Ionic UI',
-      skillPercentage: '80',
-      skillIcon: ionicIcon
+      shortSkillName: 'Cross-browser Compatibility'
+    },
+    {
+      shortSkillName: 'Theme Customization'
+    },
+    {
+      shortSkillName: 'Custom Product Properties'
+    },
+    {
+      shortSkillName: 'Website Building'
+    },
+    {
+      shortSkillName: 'Scratch Theme'
+    },
+    {
+      shortSkillName: 'Elementor Pro'
+    },
+    {
+      shortSkillName: 'Membership Systems'
+    },
+    {
+      shortSkillName: 'Paypal Integration'
+    },
+    {
+      shortSkillName: 'Document Object Model (DOM)'
+    },
+    {
+      shortSkillName: 'Problem Solving'
+    },
+    {
+      shortSkillName: 'End-to-end Testing'
+    },
+    {
+      shortSkillName: 'Adaptive Design'
+    },
+    {
+      shortSkillName: 'User Requirements'
+    },
+    {
+      shortSkillName: 'Communication'
+    },
+    {
+      shortSkillName: 'Analytical Skills'
+    },
+    {
+      shortSkillName: 'Wireframing'
+    },
+    {
+      shortSkillName: 'Bitbucket'
+    },
+    {
+      shortSkillName: 'Jira'
+    },
+    {
+      shortSkillName: 'Gitlab'
+    },
+    {
+      shortSkillName: 'GitHub'
+    },
+    {
+      shortSkillName: 'Search Engine Optimization (SEO)'
+    },
+    {
+      shortSkillName: 'Project Management'
+    },
+    {
+      shortSkillName: 'App Integration'
+    },
+    {
+      shortSkillName: 'Responsiveness'
+    },
+    {
+      shortSkillName: 'Web Architecture'
+    },
+    {
+      shortSkillName: 'XD, Figma, Zeplin, PSD to WordPress/Shopify/Custom'
+    },
+    {
+      shortSkillName: 'Single Page Applications'
+    },
+    {
+      shortSkillName: 'ACF Pro'
+    },
+    {
+      shortSkillName: 'Gutenberg Editor'
+    },
+    {
+      shortSkillName: 'Custom Gutenberg Blocks'
+    },
+    {
+      shortSkillName: 'WordPress Development'
+    },
+    {
+      shortSkillName: 'Landing Pages'
+    },
+    {
+      shortSkillName: 'Store Design'
+    },
+    {
+      shortSkillName: 'Redesigning'
+    },
+    {
+      shortSkillName: 'Shopify'
+    },
+    {
+      shortSkillName: 'New Store Development'
+    },
+    {
+      shortSkillName: 'Donation Plugin'
+    },
+    {
+      shortSkillName: 'Figma Conversion'
+    },
+    {
+      shortSkillName: 'Adobe XD'
+    },
+    {
+      shortSkillName: 'Plugin UI'
+    },
+    {
+      shortSkillName: 'Theme/Plugin Customization'
+    },
+    {
+      shortSkillName: 'Custom Theme Development'
+    },
+    {
+      shortSkillName: 'Plugins Integration'
+    },
+    {
+      shortSkillName: 'PHP'
+    },
+    {
+      shortSkillName: 'WPML'
+    },
+    {
+      shortSkillName: 'ACF'
+    },
+    {
+      shortSkillName: 'JavaScript'
+    },
+    {
+      shortSkillName: 'HTML'
+    },
+    {
+      shortSkillName: 'jQuery'
+    },
+    {
+      shortSkillName: 'Web Design'
+    },
+    {
+      shortSkillName: 'Web Development'
+    },
+    {
+      shortSkillName: 'SQL (Learning Based)'
+    },
+    {
+      shortSkillName: 'XML (Learning Based)'
+    },
+    {
+      shortSkillName: 'Cascading Style Sheets (CSS)'
+    },
+    {
+      shortSkillName: 'Java (Learning Based)'
+    },
+    {
+      shortSkillName: 'Photoshop'
+    },
+    {
+      shortSkillName: 'Microsoft Office'
+    },
+    {
+      shortSkillName: 'HTML5'
+    },
+    {
+      shortSkillName: 'WordPress'
+    },
+    {
+      shortSkillName: 'C# (Learning Based)'
+    },
+    {
+      shortSkillName: 'Ionic Framework'
+    },
+    {
+      shortSkillName: 'C++ (Learning Based)'
+    },
+    {
+      shortSkillName: 'AutoCAD (Learning Based)'
+    },
+    {
+      shortSkillName: 'Content Management Systems (CMS)'
+    },
+    {
+      shortSkillName: 'SASS'
+    },
+    {
+      shortSkillName: 'Bootstrap'
+    },
+    {
+      shortSkillName: 'Team Management'
+    },
+    {
+      shortSkillName: 'Teamwork'
+    },
+    {
+      shortSkillName: 'Interpersonal Communication'
+    },
+    {
+      shortSkillName: 'Shorthand (Learning Based)'
+    },
+    {
+      shortSkillName: 'npm'
+    },
+    {
+      shortSkillName: 'Gulp.js'
+    },
+    {
+      shortSkillName: 'Speed Optimization'
+    },
+    {
+      shortSkillName: 'E-commerce'
+    },
+    {
+      shortSkillName: 'WordPress Design'
     }
   ]
   return (
@@ -96,7 +320,7 @@ function Skills() {
                 <div className='skill-info flex items-center justify-center'><img alt='skill icon' src={skillsdetails.skillIcon} />{skillsdetails.skillname}</div>
               </div>
             ))}
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px" className='absolute'>
               <defs>
                 <linearGradient id="GradientColor">
                   <stop offset="0%" stopColor="var(--green)" />
@@ -107,6 +331,22 @@ function Skills() {
           </div>
 
 
+        </div>
+        <div className="grid grid-cols-1 items-center gap-y-8 gap-x-8 sm:gap-y-16 lg:grid-cols-1">
+          <div className='additional-skills-tags'>
+            <h2 className='mt-20 text-[1rem] md:text-[2rem] font-black text-[var(--green)]'> Additional Skills</h2>
+            <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+              {
+                additionalSkills.map((as, i) => (
+                  <li className="mr-1.5 mt-2" key={i}>
+                    <div
+                      className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-md font-medium leading-5 text-teal-300">
+                      {as.shortSkillName}</div>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
         </div>
       </div>
     </div>
