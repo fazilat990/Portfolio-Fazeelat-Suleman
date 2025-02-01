adc333479e528ff89ba6d32450141f0ff00ad523
fazilat990 <59200609+fazilat990@users.noreply.github.com>
2025-02-01 23:36:46 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.png';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='header-wrap relative z-10'>
       <div className="header-2">
@@ -15,13 +16,13 @@ function Header() {
               <Link to="/" className="font-bold text-xl text-indigo-600">
                 <img src={logo} alt='Logo' width='200' />
               </Link>
-              <button className="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle">
+              <button className="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle" onClick={() => setIsOpen(!isOpen)}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="hamburger-menu" width="20px" height="30px" fill='#fff'>
                   <path d="M5 7h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1zm0 6h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1zm0 6h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1z"></path>
                 </svg>
               </button>
             </div>
-            <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+            <div className={`${isOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0`} id="navbar-collapse">
               <Link to="/about" className="p-2 lg:px-4 md:mx-2 text-white rounded font-black hover:text-[var(--green)] transition-colors duration-300">About</Link>
               <Link to="/skills" className="p-2 lg:px-4 md:mx-2 text-white rounded font-black hover:text-[var(--green)] transition-colors duration-300">Skills</Link>
               <Link to="/work" className="p-2 lg:px-4 md:mx-2 text-white rounded font-black hover:text-[var(--green)] transition-colors duration-300">Work</Link>
